9ca2a7c472ee230c44509b9d0e959b182a4d92c3
Romario Gustave <romagustave10@gmail.com>
2020-01-10 00:29:39 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,4 +7,29 @@ class Triangle
     end 
     def valid? 
       sum_one_two = @triangle_sides[0] + @triangle_sides[1]
-      sum_one_three = @triangle_sides[0] + @triangle_sides[2] sum_two_three = @triangle_sides[1] + @triangle_sides[2] if (@triangle_sides.none? {|side| side <= 0}) && (sum_one_two > @triangle_sides[2] && sum_one_three > @triangle_sides[1] && sum_two_three > @triangle_sides[0]) return true else return false end end def kind if valid? if @triangle_sides.uniq.length == 1 return :equilateral elsif @triangle_sides.uniq.length == 2 return :isosceles else return :scalene end else raise TriangleError end endendclass TriangleError < StandardErrorend
+      sum_one_three = @triangle_sides[0] + @triangle_sides[2] 
+      sum_two_three = @triangle_sides[1] + @triangle_sides[2] 
+      if (@triangle_sides.none? {|side| side <= 0}) && (sum_one_two > @triangle_sides[2] && sum_one_three > @triangle_sides[1] && sum_two_three > @triangle_sides[0]) 
+        return true 
+        else 
+          return false 
+          end 
+          end 
+          def kind 
+            if valid? 
+              if @triangle_sides.uniq.length == 1 
+                return :equilateral 
+                elsif @triangle_sides.uniq.length == 2 
+                return :isosceles 
+                else 
+                  return :scalene 
+                  end 
+                  else 
+                    raise TriangleError 
+                    end 
+                    end
+                    end
+                    
+                    class Triangle
+                    class TriangleError < StandardError
+                      end
